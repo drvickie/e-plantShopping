65a9d30ea865f0704f2a8dadc1af4eb0ac8c8bb7
DrVickie <116500226+drvickie@users.noreply.github.com.>
2026-03-18 12:21:54 +0000

    Fixing the home page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import AboutUs from "./components/AboutUs";
 import ProductList from "./components/ProductList";
 import CartItem from "./components/CartItem";
 import Header from "./components/Header";
 
-
 function App() {
-
   return (
     <Router>
       <Routes>
         <Route
           path="/"
           element={
-            <div className="landing">
-              <h1>Paradise Nursery</h1>
-              <p>Bring nature home 🌱</p>
-              <Link to="/plants">
-                <button>Get Started</button>
-              </Link>
-            </div>
+            <>
+              <Header />
+
+              <div className="home">
+                <img
+                  src="https://images.unsplash.com/photo-1501004318641-b39e6451bec6"
+                  alt="plants"
+                  className="home-image"
+                />
+
+                <h1>Paradise Nursery</h1>
+                <p>Bring nature into your home with our beautiful plants 🌱</p>
+
+                <Link to="/plants">
+                  <button className="home-btn">Get Started</button>
+                </Link>
+              </div>
+            </>
           }
         />
+
         <Route path="/plants" element={<ProductList />} />
         <Route path="/cart" element={<CartItem />} />
       </Routes>
